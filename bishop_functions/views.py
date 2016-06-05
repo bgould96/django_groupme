9f14d40047df0b08a8c13c43191a636994f0288f
Bobby Gould <bgould96@gmail.com>
2016-06-05 14:47:37 -0500

    no message

--- bishop_functions/views.py
+++ bishop_functions/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+import requests, json
+
+required_ids = [""]
+
+@csrf_exempt
+def index(request):
+        print request
+
+
+
+        return HttpResponse("Users updated.")
