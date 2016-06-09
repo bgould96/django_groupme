5aa99629dacd88cbcb594871d3ec591eed0e4f3d
Bobby <bgould96@gmail.com>
2016-06-08 19:41:10 -0500

    no message

--- crocs/views.py
+++ crocs/views.py
@@ -9,6 +9,4 @@ from django.views.decorators.csrf import csrf_exempt
 import requests, json
 
 def index(request):
-    test = request["response"];
-
-    return HttpResponse("<p>" + test + "</p>");
+    return HttpResponse("<p>" + request + "</p>");
