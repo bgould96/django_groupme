691657f5bb863e1a6cf863166106fe758742cc31
Bobby <bgould96@gmail.com>
2016-06-09 22:40:56 -0500

    no message

--- add_bot/tests.py
+++ add_bot/tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin python3
 
 from django.test import TestCase
 
--- groupme/settings.py
+++ groupme/settings.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin python3
 
 """
 Django settings for groupme project.
